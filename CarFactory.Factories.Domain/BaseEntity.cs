ff60dae8fa2b69065eac925daeb7e66b94e808f5
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-02-17 19:19:35 +0100

    WIP: Implement Base Entity for all models

---
+++ CarFactory.Factories.Domain/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CarFactory.Factories.Domain;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string CreatedBy { get; set; } = "SYSTEM";
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+}
