9d20cb8e67171ad39710dc832ad2a0deb785371a
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-02-17 20:23:03 +0100

    WIP: Extend Business model

--- CarFactory.Factories.Domain/Models/Business.cs
+++ CarFactory.Factories.Domain/Models/Business.cs
@@ -1,8 +1,11 @@
-﻿namespace CarFactory.Factories.Domain.Models;
+﻿using CarFactory.Factories.Domain.ValueObjects;
+
+namespace CarFactory.Factories.Domain.Models;
 
 public class Business : BaseEntity
 {
     public required string Name { get; set; }
     public required string Customer { get; set; }
     public List<Factory> Factories { get; set; } = [];
+    public List<Employee> Employees { get; set; } = [];
 }
