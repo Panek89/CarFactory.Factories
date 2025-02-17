f62fe46c9afcb78f3ab3346298de18ac23e3274d
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-02-17 20:24:07 +0100

    WIP: Add ValueObject for Employee representation

---
+++ CarFactory.Factories.Domain/ValueObjects/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarFactory.Factories.Domain.ValueObjects;
+
+public class Employee
+{
+    public required string EvidenceNo { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+
+    [NotMapped]
+    public string FullName => $"{FirstName} {LastName}";
+    [NotMapped]
+    public string FullNameReverse => $"{LastName} {FirstName}";
+}
