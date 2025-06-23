3879dfe7065505e90a0b12fa1383cd3dbdc73e00
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-23 20:22:33 +0200

    WIP: Entity Factory

--- src/factories/entities/factory.entity.ts
+++ src/factories/entities/factory.entity.ts
@@ -1,1 +1,13 @@
-export class Factory {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Factory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  city: string;
+}
