15a8f7a48b625581bfb4b34a23823cb24c51da4e
Kamil Pankowski <pankowski.kamil1989@gmail.com>
2025-06-23 18:44:25 +0200

    WIP: Generate Resource Factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FactoriesController } from './factories/factories.controller';
+import { FactoriesModule } from './factories/factories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [FactoriesModule],
+  controllers: [AppController, FactoriesController],
   providers: [AppService],
 })
 export class AppModule {}
