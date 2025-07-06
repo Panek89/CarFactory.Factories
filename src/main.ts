44bb81c90a8893a8396f96e08bf74caf128028ef
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-07-06 10:58:35 +0200

    WIP: Inject and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CarFactory Factories')
+    .setDescription('Factories microservice API documentation from CarFactory application')
+    .setVersion('1.0')
+    .addTag('factories')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
