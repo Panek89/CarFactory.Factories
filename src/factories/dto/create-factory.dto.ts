dacc73454af5fdca0619f00493f000f0a0a3110b
Kamil-T480 <pankowski.kamil1989@gmail.com>
2025-07-06 12:00:15 +0200

    WIP: Example Create Factory DTO values

--- src/factories/dto/create-factory.dto.ts
+++ src/factories/dto/create-factory.dto.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateFactoryDto {
+  @ApiProperty({ example: 'BMW Group Werk Leipzig' })
   name: string;
+
+  @ApiProperty({ example: 'Leipzig' })
   city: string;
+
+  @ApiProperty({ example: 250 })
   numberOfEmployees: number;
+
+  @ApiProperty({ example: true })
   isOpen: boolean;
+
+  @ApiProperty({ example: '2023-01-15T08:00:00.000Z' })
   openDate: Date;
-  closeDate: Date;
+
+  @ApiProperty({ example: null, nullable: true })
+  closeDate?: Date;
 }
